25efa25b14812d383a96ec2e6e32e9b2703dfb6c
aburu <aburushed@gmail.com>
2022-07-22 00:51:54 +0600

    Drawer with listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+//import 'dart:html';
+
 import 'dart:html';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutterapp/widgets/my_custom_tile.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main(){
   runApp(MyApp());
@@ -8,19 +13,120 @@ void main(){
 }
 class Home extends StatelessWidget {
 
+  List<String> userList = ['Home Page','Category','Favorite'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(
-        backgroundColor: Colors.blue,
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: ListView(
+         children: [
+           const UserAccountsDrawerHeader(
+             accountName: const Text("Abu Rushed"),
+             accountEmail: const Text("aburushed@gmail.com"),
+             currentAccountPicture: CircleAvatar(
+               child:Icon(Icons.person),
+             ),
+             
+           ),
+          MyCustomTile(title:userList[0],icon: null,onClickEvent:(){
+            Navigator.pop(context);
+            Fluttertoast.showToast(msg:userList[0]);
+
+          },
+            subtitle: "30 min ago",
+            trailing: "10",
+          ),
+
+           MyCustomTile(title:userList[1],icon: null,onClickEvent:(){
+             Navigator.pop(context);
+           }),
+
+           ListTile(
+             onTap: (){
+              Navigator.pop(context);//drawar back
+             },
+
+             title: Text(userList[0]),
+             leading: CircleAvatar(
+               //child: Text("H"),
+               child: Icon(Icons.person),
+             ),
+             trailing: Text("10"),
+
+           ),
+           const Divider(height:1,),
+           ListTile(
+             onTap: (){
+               Navigator.pop(context);//drawar back
+             },
+
+             title: Text(userList[1]),
+             leading: CircleAvatar(
+               child: Text("C"),
+               //child: Icon(Icons.person),
+             ),
+             trailing: Text("10"),
+
+           ),
+           const Divider(height:1,),
+           ListTile(
+             onTap: (){
+               Navigator.pop(context);//drawar back
+             },
+
+             title: Text(userList[2]),
+             leading: CircleAvatar(
+               child: Text("F"),
+               //child: Icon(Icons.person),
+             ),
+             trailing: Text("10"),
+             subtitle: Text("Favourite"),
+
+           )
+
+         ],
+        ),
       ),
+
       appBar: AppBar(
         title:const Text("Home Screen"),
+        actions: [
+          IconButton(
+            onPressed:()
+            {
+              Fluttertoast.showToast(msg: 'Searching......');
+            },
+            icon: Icon(Icons.search_outlined),
+          ),
+          IconButton(
+            onPressed:()
+            {
+
+            },
+            icon: Icon(Icons.person_outlined),
+          ),
+          IconButton(
+            onPressed:()
+            {
+              print("Shoping Cart Clicked");
+              Fluttertoast.showToast(msg: 'Shoping Cart');
+            },
+            icon: Icon(Icons.shopping_cart_outlined),
+
+          )
+        ],
+
+
+        elevation: 10.0,
         backgroundColor: Colors.deepOrange,
+
       ),
       body: Container(
         color:Colors.black45
       ),
+
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.red,
         onPressed: (){
@@ -42,7 +148,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Home(),
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       title: "Hello",
       theme: ThemeData(
         brightness:  isDart==false? Brightness.light:Brightness.dark,
---
+++ lib/widgets/my_custom_tile.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+class MyCustomTile extends StatelessWidget {
+  String title;
+  String? subtitle;
+  IconData? icon;
+  String? trailing;
+  VoidCallback onClickEvent;
+
+  MyCustomTile(
+      { this.icon, required this.onClickEvent, this.subtitle, required this.title, this.trailing});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onClickEvent,
+
+      title: Text(title!),
+      leading: CircleAvatar(
+        //child: Text("H"),
+        //child: Icon(icon),
+        child: icon == null?Text(title[0]):Icon(icon),
+      ),
+      trailing: Text(trailing??''),
+
+    );
+  }
+}
